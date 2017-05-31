0a3228e62667355746443ac212f42c74cc951669
Prashant-Naicker <prashantnaicker@gmail.com>
2017-05-31 13:24:14 +1200

    Added Signup activity

--- src/android/Messenger/app/src/main/java/com/example/prashant/messenger/LoginActivity.java
+++ src/android/Messenger/app/src/main/java/com/example/prashant/messenger/LoginActivity.java
@@ -1,13 +1,32 @@
 package com.example.prashant.messenger;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+    TextView tvSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        tvSignUp = (TextView) findViewById(R.id.tvSignUp);
+
+        tvSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchSignupActivity();
+            }
+        });
+    }
+
+    private void launchSignupActivity() {
+        Intent intent = new Intent(this, SignupActivity.class);
+        startActivity(intent);
     }
 }
+
