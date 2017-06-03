14222cd7e436e57bf680725b714ff3712a893caa
Prashant-Naicker <prashantnaicker@gmail.com>
2017-06-03 22:24:54 +1200

    Changing structure

--- src/android/Messenger/app/src/main/java/com/example/prashant/messenger/LoginActivity.java
+++ src/android/Messenger/app/src/main/java/com/example/prashant/messenger/LoginActivity.java
@@ -7,26 +7,12 @@ import android.view.View;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
-    TextView tvSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-        tvSignUp = (TextView) findViewById(R.id.tvSignUp);
-
-        tvSignUp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                launchSignupActivity();
-            }
-        });
     }
 
-    private void launchSignupActivity() {
-        Intent intent = new Intent(this, SignupActivity.class);
-        startActivity(intent);
-    }
 }
 
