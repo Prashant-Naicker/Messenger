8e062f656b5645a7e448919d4d8c94f9b81dec57
Prashant-Naicker <prashantnaicker@gmail.com>
2017-06-05 16:28:35 +1200

    Added root and server keys/certs

--- src/golang/main.go
+++ src/golang/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-    err := http.ListenAndServeTLS(":8080", "key.pem", "cert.pem", nil)
+    err := http.ListenAndServeTLS(":8080", "../src/golang/crt/messenger.jobjot.co.nz.crt", "../src/golang/key/messenger.jobjot.co.nz.key", nil)
     if err != nil {
         fmt.Println(err)
         return
