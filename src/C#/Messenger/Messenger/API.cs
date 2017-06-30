83ca0559bb3b4608dea0b2d410f6931d6c292777
Prashant-Naicker <prashantnaicker@gmail.com>
2017-06-30 13:47:59 +1200

    Added to API class

--- src/C#/Messenger/Messenger/API.cs
+++ src/C#/Messenger/Messenger/API.cs
@@ -1,12 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Newtonsoft.Json.Linq;
 
 namespace Messenger
 {
     class API
     {
+        private static string _url;
+        private JObject _reqObj;
+
+        public API(string url)
+        {
+            _url = url;
+        }
+
+        // Request/Response.
+        public void setRequestObject(JObject reqObj)
+        {
+            _reqObj = reqObj;
+        }
     }
 }
