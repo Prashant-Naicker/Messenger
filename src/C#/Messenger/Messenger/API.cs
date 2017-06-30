27eea1637cc84a2eda4e3d68a18566db3017d6c9
Prashant-Naicker <prashantnaicker@gmail.com>
2017-06-30 16:53:10 +1200

    Finished most of API class

--- src/C#/Messenger/Messenger/API.cs
+++ src/C#/Messenger/Messenger/API.cs
@@ -1,8 +1,12 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Threading;
 
 namespace Messenger
 {
-    class API
+    abstract class API
     {
         private static string _url;
         private JObject _reqObj;
@@ -16,6 +20,35 @@ namespace Messenger
         public void setRequestObject(JObject reqObj)
         {
             _reqObj = reqObj;
+        }
+        private void raiseResponse(Exception ex, string resObj)
+        {
+            onResponseCallback(ex, resObj);
+        }
+        public abstract void onResponseCallback(Exception ex, string resObj);
+
+        // Send
+        public void send()
+        {
+            ThreadStart ts = new ThreadStart(() => commenceRequestSend());
+            Thread t = new Thread(ts);
+            t.Start();
+        }
+
+        public void commenceRequestSend()
+        {
+           using (var client = new HttpClient())
+            {
+                try
+                {
+                    HttpResponseMessage resObj = client.PostAsync(_url, new StringContent(_reqObj.ToString(), System.Text.Encoding.UTF8, "application/json")).Result;
+                    raiseResponse(null, JsonConvert.SerializeObject(resObj));
+                }
+                catch (Exception ex)
+                {
+                    raiseResponse(ex, null);
+                }
+            }
         }
     }
 }
